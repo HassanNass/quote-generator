3061cdc377ebd5479d80f1857eb925b7d5882193
Hassan Nass <hassannass613@gmail.com>
2026-06-12 14:25:41 +0300

    add quote fetching with useEffect and auto-refresh interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import "./App.css"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [quote, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function fetchQuote() {
+    try{
+      setIsLoading(true)
+      const response = await fetch(`https://dummyjson.com/quotes/random`)
+      const data = await response.json()
+      setQuote(data.quote)
+      setAuthor(data.author)
+      setIsLoading(false)
+    }
+    catch{
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuote()
+    const interval = setInterval(() => {
+      fetchQuote()
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
   
   return(
     <div className="container">
       <h1 className="header">Random Quote Generator</h1>
+      <p className="quote">{quote}</p>
+      <p className="author"><strong>{author}</strong></p>
+      {isLoading && <p className="loading-masg">Loading...</p>}
+      <button className="btn-random"
+              onClick={fetchQuote}
+      >Generate</button>
     </div>
   )
 }
