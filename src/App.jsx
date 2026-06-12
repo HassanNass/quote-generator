9098211ede6f318f6eb4b7f6977e8623c4e80879
Hassan Nass <hassannass613@gmail.com>
2026-06-12 13:21:19 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  
+  return(
+    <div className="container">
+      <h1 className="header">Random Quote Generator</h1>
+    </div>
+  )
+}
+
+export default App
